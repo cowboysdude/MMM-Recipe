9d9ae488fa4401911fd700353d93afad9f4ce467
John Wade <johnw8017@gmail.com>
2019-01-01 18:51:07 -0500

    Update node_helper.js

--- node_helper.js
+++ node_helper.js
@@ -1,68 +1,96 @@
 /* Magic Mirror
-    * Module: MMM-Recipe
-    *
-    * By Cowboysdude/ node_helper written by: strawberry 3.141
-    * 
-    */
+ * Module: MMM-Recipe
+ *
+ * By Cowboysdude
+ * 
+ */
 const NodeHelper = require('node_helper');
-const request = require('request');
-const fs = require('fs');
+const request = require('request'); 
 
 module.exports = NodeHelper.create({
 
     start: function() {
-        this.recipe = {
-            timestamp: null,
-            data: null
-        };
-        this.path = "modules/MMM-Recipe/recipe.json";
-        if (fs.existsSync(this.path)) {
-            var temp = JSON.parse(fs.readFileSync(this.path, 'utf8'));
-            if (temp.timestamp === this.getDate()) {
-                this.recipe = temp;
-            }
-            //console.log(temp);
-        }
-
+        console.log('starting Node_helper for ' + this.name +" "+ this.data); 
     },
 
     getRecipe: function(url) {
         request({
-            url: url,
+            url: "http://www.themealdb.com/api/json/v1/1/random.php",
             method: 'GET'
         }, (error, response, body) => {
             if (!error && response.statusCode == 200) {
-                var result = JSON.parse(body).meals[0];
-                this.sendSocketNotification('RECIPE_RESULT', result);
-                this.recipe.timestamp = this.getDate();
-                this.recipe.data = result;
-                this.fileWrite();
+                var result = JSON.parse(body);
+                //console.log(result); 
+                // Just setting it all up to start.
+                var meals = result.meals; //returns array
+                var meal = meals[0]; // returns object
+                //console.log(meal);
+                // Start here to rename keys and match them to the ingredients.
+				//result.meals.forEach(meal => Object.keys(meal).forEach(key => console.log(meal[key])));
+                const newArray = meals.reduce((array, meal) => {
+                    // Rather than iterate over ALL of the keys, just 
+                    // do this, basically 50% of the keys. 
+                    const subArray = Object.keys(meal).filter(key => key.indexOf('strIngredient' == -1));
+                    // console.log(subArray);
+                    // Basically add some ojects to the array.
+                    subArray.forEach(key => {
+                        const int = key.replace(/\D/g, '');
+                        const measureKey = `strMeasure${int}`;
+                        const ingredientKey = `strIngredient${int}`;  
+					//console.log(ingredientKey);
+                        const obj = {
+                            measure: meal[measureKey],
+                            ingredient: meal[ingredientKey]
+                        };
+                         // console.log(obj);  //Testing data before
+                         
+						if (obj.measure && obj.ingredient != 'undefined' || undefined || '' || null) {
+                         array.push(obj);
+                              console.log(array);  //Testing data after
+                        }
+                    });
+
+                    const recipeName = meal.strMeal;
+                    const instruction = meal.strInstructions;
+                    const video = meal.strYoutube;
+                    const thumb = meal.strMealThumb;
+                    const nation = meal.strArea;
+                    const category = meal.strCategory;
+
+                    const recipe = {instructions: {
+                        recipeName,
+                        instruction,
+                        video,
+                        thumb,
+                        nation,
+                        category
+                    } };
+                    array.push(recipe);
+                    //console.log(recipe);  Testing full array
+
+                    // Make sure to return the array.
+                    return array;
+
+                }, []);
+
+                // Now just print the resuts, and make sure that you know 
+                // and alert that the app has finished.
+
+
+                //console.log(newArray, "FINISHED");
+
+                this.sendSocketNotification('RECIPE_RESULT', newArray);
             }
         });
-    },
-
-    fileWrite: function() {
-        fs.writeFile(this.path, JSON.stringify(this.recipe), function(err) {
-            if (err) {
-                return console.log(err);
-            }
-            console.log("The Recipe file was saved!");
-        });
-    },
-
-    getDate: function() {
-        return (new Date()).toLocaleDateString();
     },
 
     //Subclass socketNotificationReceived received.
     socketNotificationReceived: function(notification, payload) {
         if (notification === 'GET_RECIPE') {
-            if (this.recipe.timestamp === this.getDate() && this.recipe.data !== null) {
-                this.sendSocketNotification('RECIPE_RESULT', this.recipe.data);
-            } else {
-                this.getRecipe(payload);
-            }
+
+            this.getRecipe(payload);
         }
     }
+
 
 });

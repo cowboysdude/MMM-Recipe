190d413ce4e6cf96d7fd5dbde0d43972f911a5df
John Wade <johnw8017@gmail.com>
2019-01-02 21:57:01 -0500

    Update node_helper.js

--- node_helper.js
+++ node_helper.js
@@ -5,12 +5,12 @@
  * 
  */
 const NodeHelper = require('node_helper');
-const request = require('request'); 
+const request = require('request');
 
 module.exports = NodeHelper.create({
 
     start: function() {
-        console.log('starting Node_helper for ' + this.name +" "+ this.data); 
+        console.log('starting Node_helper for ' + this.name + " " + this.data);
     },
 
     getRecipe: function(url) {
@@ -19,69 +19,36 @@ module.exports = NodeHelper.create({
             method: 'GET'
         }, (error, response, body) => {
             if (!error && response.statusCode == 200) {
-                var result = JSON.parse(body);
-                //console.log(result); 
-                // Just setting it all up to start.
-                var meals = result.meals; //returns array
-                var meal = meals[0]; // returns object
-                //console.log(meal);
-                // Start here to rename keys and match them to the ingredients.
-				//result.meals.forEach(meal => Object.keys(meal).forEach(key => console.log(meal[key])));
-                const newArray = meals.reduce((array, meal) => {
-					
-                    // Rather than iterate over ALL of the keys, just 
-                    // do this, basically 50% of the keys. 
-                    const subArray = Object.keys(meal).filter(key => key.indexOf('strIngredient' == -1));
-                     //console.log(subArray); //this returns 1 of each
-                    // Basically add some ojects to the array.
-					
-		//I'm sure the problem is the next line but not sure how to fix it........ 
-					subArray.forEach(key => {
-		//Tried replacing it with a loop but apparently didn't get it right......
-                        const int = key.replace(/\D/g, '');
-						//console.log(int);
-                        const measureKey = `strMeasure${int}`;
-                        const ingredientKey = `strIngredient${int}`;  
-					console.log(ingredientKey);
-                        const obj = {
-                            measure: meal[measureKey],
-                            ingredient: meal[ingredientKey]
-                        };
-                       // console.log(obj);  //Testing data before
-                        
-						// getting rid of blanks 
-						if (obj.measure && obj.ingredient != 'undefined' || undefined || '' || null) {
-						 array.push(obj);
-                          //    console.log(array);  //Testing data after
-                        }
-                    });  
-                    // Make sure to return the array.
-                    return array;
+                var result = JSON.parse(body),
+                    meal = result.meals[0];
 
-                }, []); 
-				
-			 //just adding whats remaining to the new array......
-               
-                    const recipeName = meal.strMeal;
-                    const instruction = meal.strInstructions;
-                    const video = meal.strYoutube;
-                    const thumb = meal.strMealThumb;
-                    const nation = meal.strArea;
-                    const category = meal.strCategory;
+                function youtube_parser(url) {
+                    var regExp = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#\&\?]*).*/;
+                    var match = url.match(regExp);
+                    return (match && match[7].length == 11) ? match[7] : false;
+                }
 
-                    const recipe =  {
-                        recipeName,
-                        instruction,
-                        video,
-                        thumb,
-                        nation,
-                        category
-                    };
-                    newArray.push(recipe);
-					 
-           //    console.log(newArray, "FINISHED");
+                var recipe = {
+                    recipeName: meal.strMeal,
+                    instruction: meal.strInstructions,
+                    videoId: youtube_parser(meal.strYoutube),
+                    video: meal.strYoutube,
+                    thumb: meal.strMealThumb,
+                    nation: meal.strArea,
+                    category: meal.strCategory,
+                    ingredients: []
+                };
 
-                this.sendSocketNotification('RECIPE_RESULT', newArray);
+                for (var i = 1; i <= 24; i++) {
+                    if (!meal['strIngredient' + i]) {
+                        break;
+                    }
+                    recipe.ingredients.push({
+                        ingredient: meal['strMeasure' + i] + " " + meal['strIngredient' + i]
+                    });
+                }
+                
+                this.sendSocketNotification('RECIPE_RESULT', recipe);
             }
         });
     },
@@ -93,6 +60,4 @@ module.exports = NodeHelper.create({
             this.getRecipe(payload);
         }
     }
-
-
 });

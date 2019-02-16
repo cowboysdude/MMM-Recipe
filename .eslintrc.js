ad8055db942c63ee3e39781ec79f8ffea970f1f2
John Wade <johnw8017@gmail.com>
2019-02-15 21:28:55 -0500

    Add files via upload

---
+++ .eslintrc.js
@@ -0,0 +1,41 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "commonjs": true,
+        "es6": true,
+        "node": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": 2018,
+        "sourceType": "module"
+    },
+    "rules": {
+        "indent": [
+            "error",
+            "tab"
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ],
+		"comma-dangle": 0, 
+        "no-console": 0  
+	},
+    "globals": {
+    "moment": true,
+	"Module": true,
+	"log": true,
+	"Log": true,
+	"config": true
+    
+    }
+};
